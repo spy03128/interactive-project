ca2ed6f9c6e728793c11d014c64fcbf33fd86114
spy03128 <spy03128@naver.com>
2021-03-02 23:31:38 +0900

    currentTarget(this)와 target 이벤트

--- script/ilbuni.js
+++ script/ilbuni.js
@@ -1,22 +1,33 @@
 window.addEventListener('DOMContentLoaded',function(){
-  const ilbuni = document.querySelector('.ilbuni.c');
+  // const ilbuni = document.querySelector('.ilbuni.c');
+  //
+  // function clickInbuntilHandler(){
+  //   ilbuni.classList.toggle('special');
+  // }
+  //
+  // ilbuni.addEventListener('click',clickInbuntilHandler);
+  //
+  //
+  // const ilbuni2 = document.querySelector('.ilbuni.a');
+  // function clickHandler(){
+  //   ilbuni2.classList.toggle('plus');
+  // }
+  // ilbuni2.addEventListener('mousedown',clickHandler);
+  //
+  // const ilbuni3 = document.querySelector('.ilbuni.b');
+  // function keydownHandler(){
+  //   ilbuni3.classList.toggle('key');
+  // }
+  // ilbuni3.addEventListener('mouseover',keydownHandler);
 
-  function clickInbuntilHandler(){
-    ilbuni.classList.toggle('special');
+
+  const ilbuni = document.querySelector('.characters');
+
+  function clickHandler(e){
+    console.log(this);
+    console.log(e.currentTarget); //같은 표현
+    console.log(e.target);
   }
 
-  ilbuni.addEventListener('click',clickInbuntilHandler);
-
-
-  const ilbuni2 = document.querySelector('.ilbuni.a');
-  function clickHandler(){
-    ilbuni2.classList.toggle('plus');
-  }
-  ilbuni2.addEventListener('mousedown',clickHandler);
-
-  const ilbuni3 = document.querySelector('.ilbuni.b');
-  function keydownHandler(){
-    ilbuni3.classList.toggle('key');
-  }
-  ilbuni3.addEventListener('mouseover',keydownHandler);
+  ilbuni.addEventListener('click',clickHandler);
 });
