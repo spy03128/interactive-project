aa5645db9565a57e332a41d8abd0cd2163f1b6b6
spy03128 <spy03128@naver.com>
2021-03-04 21:36:40 +0900

    이벤트 위임 코드 작성

--- script/move.js
+++ script/move.js
@@ -1,13 +1,35 @@
+// window.addEventListener("DOMContentLoaded", function() {
+//   const ilbuniGroup = document.querySelectorAll(".ilbuni");
+//   const stage = document.querySelector(".stage");
+//
+//   function clickHandler() {
+//     // stage.removeChild(this);
+//     this.parentNode.removeChild(this);
+//   }
+//
+//   // for (let i = 0; i < ilbuniGroup.length; i++) {
+//   //   ilbuniGroup[i].addEventListener("click", clickHandler);
+//   // }
+//
+//   //for문 풀어쓴것
+//   ilbuniGroup[0].addEventListener("click", clickHandler);
+//   ilbuniGroup[1].addEventListener("click", clickHandler);
+//   ilbuniGroup[2].addEventListener("click", clickHandler);
+//
+//
+//
+// });
+
+//이벤트 위임으로 다시 짠 코드 : 부모한테 위임 <stage 이용>
 window.addEventListener("DOMContentLoaded", function() {
-  const ilbuniGroup = document.querySelectorAll(".ilbuni");
   const stage = document.querySelector(".stage");
 
-  function clickHandler() {
-    // stage.removeChild(this);
-    this.parentNode.removeChild(this);
+  function clickHandler(e) {
+    if (e.target.classList.contains("ilbuni")) {
+      // console.log(e.target);
+      stage.removeChild(e.target);
+    }
   }
 
-  for (let i = 0; i < ilbuniGroup.length; i++) {
-    ilbuniGroup[i].addEventListener("click", clickHandler);
-  }
+  stage.addEventListener("click", clickHandler); //한번만 사용가능. 이전에는 모든 요소에 사용해야하였음.
 });
