f57b1962a9d26ded59f0d54a8f5515dd9f31eaee
spy03128 <spy03128@naver.com>
2021-03-06 02:11:14 +0900

    예제 업그레이드: 활성화,비활성화 함수 구현

--- script/door.js
+++ script/door.js
@@ -2,19 +2,29 @@ window.addEventListener("DOMContentLoaded", function() {
   const stageElem = document.querySelector(".stage");
   let currentItem = null; //현재 활성화된 아이템을 저장하는 변수
 
+  function activate(elem) {
+    elem.classList.add("door-opened");
+    currentItem = elem;
+  }
+
+  function inactivate(elem) {
+    elem.classList.remove("door-opened");
+  }
+
   function doorHandler(e) {
     const targetElem = e.target;
-
     // const currentItem = document.querySelector(".door-opened");
 
     if (currentItem) {
-      currentItem.classList.remove("door-opened");
+      inactivate(currentItem);
     }
     if (targetElem.classList.contains("door-body")) {
-      targetElem.parentNode.classList.add("door-opened");
-      currentItem = targetElem.parentNode;
+      activate(targetElem.parentNode);
     }
   }
 
   stageElem.addEventListener("click", doorHandler);
+
+  // activate(document.querySelector(".door:first-child"));
+  activate(document.querySelector(".door:nth-child(1)"));
 });
