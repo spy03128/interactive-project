3aacaf90c6828b70fca1b2a4a63e8ed9dfba08bd
spy03128 <spy03128@naver.com>
2021-03-13 17:29:32 +0900

    resize 이벤트 사용

--- script/3d.js
+++ script/3d.js
@@ -1,9 +1,17 @@
 (function() {
   const houseElem = document.querySelector(".house");
-  let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  let maxScrollValue = 0;
+
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
 
   window.addEventListener("scroll", function() {
     const zMove = (pageYOffset / maxScrollValue) * 980 - 490;
     houseElem.style.transform = "translateZ(" + zMove + "vw)";
   });
+
+  window.addEventListener("resize", resizeHandler);
+  // 처음에한번실행
+  resizeHandler();
 })();
