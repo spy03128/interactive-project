af9bf8ec5c86643e725a344ef89b7c37c050dae1
spy03128 <spy03128@naver.com>
2021-03-13 17:35:38 +0900

    scroll bar 채우기

--- script/3d.js
+++ script/3d.js
@@ -1,5 +1,6 @@
 (function() {
   const houseElem = document.querySelector(".house");
+  const barElem = document.querySelector(".progress-bar");
   let maxScrollValue = 0;
 
   function resizeHandler() {
@@ -7,8 +8,12 @@
   }
 
   window.addEventListener("scroll", function() {
-    const zMove = (pageYOffset / maxScrollValue) * 980 - 490;
+    const scrollPer = pageYOffset / maxScrollValue;
+    const zMove = scrollPer * 980 - 490;
     houseElem.style.transform = "translateZ(" + zMove + "vw)";
+
+    // progress bar
+    barElem.style.width = scrollPer * 100 + "%";
   });
 
   window.addEventListener("resize", resizeHandler);
