a08ec4009e77cc21eb0a1a723a749bc5cafa74ab
spy03128 <spy03128@naver.com>
2021-03-14 00:42:29 +0900

    마우스 움직임에 따라 시점 이동하기

--- script/3d.js
+++ script/3d.js
@@ -1,6 +1,8 @@
 (function() {
   const houseElem = document.querySelector(".house");
   const barElem = document.querySelector(".progress-bar");
+  const stageElem = document.querySelector(".stage");
+  const mousePos = { x: 0, y: 0 };
   let maxScrollValue = 0;
 
   function resizeHandler() {
@@ -16,7 +18,15 @@
     barElem.style.width = scrollPer * 100 + "%";
   });
 
+  window.addEventListener("mousemove", function(e) {
+    mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+    mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+    // console.log(mousePos.x, mousePos.y);
+    stageElem.style.transform =
+      "rotateX(" + mousePos.y * 5 + "deg) rotateY(" + mousePos.x * 5 + "deg)";
+  });
+
   window.addEventListener("resize", resizeHandler);
-  // 처음에한번실행
+  // 처음에한번실행 s
   resizeHandler();
 })();
