e5e4e82302cd27dde9a8f6864661b1f6d966dcbf
spy03128 <spy03128@naver.com>
2021-03-13 17:13:05 +0900

    스크롤에 따라 z축 값 결정하기

---
+++ script/3d.js
@@ -0,0 +1,9 @@
+(function() {
+  const houseElem = document.querySelector(".house");
+  let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+
+  window.addEventListener("scroll", function() {
+    const zMove = (pageYOffset / maxScrollValue) * 980 - 490;
+    houseElem.style.transform = "translateZ(" + zMove + "vw)";
+  });
+})();
